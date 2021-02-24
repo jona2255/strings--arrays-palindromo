39ae54758893d271c4051f0f35c4b17b349813e4
jona2255 <33894548+jona2255@users.noreply.github.com>
2021-02-24 10:11:01 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,9 +3,12 @@ function esPalindromo(frase) {
   fraseModificada = fraseModificada.replace(/[.,:; ()_ ?¿!¡-\s]/g, "");
   fraseModificada = fraseModificada.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
   let fraseAlReves = "";
+  fraseAlReves = frasseModificada.split("").reverse().join();
+  /* Versión cutre                            
   for (let index = fraseModificada.length - 1; index >= 0; index--) {
     fraseAlReves += fraseModificada[index];
   }
+  */
   console.log(fraseModificada);
   console.log(fraseAlReves);
   return fraseModificada === fraseAlReves;
